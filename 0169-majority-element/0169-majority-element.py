0276ee3a7bc0fcd53c575ec231c624cb16e19ee7
Shiv-dev2807 <shivprasaddevkatte2807@gmail.com>
2026-06-26 13:51:45 +0530

    Time: 7 ms (57.4%), Space: 21.2 MB (19.91%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,6 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        h = {}
+        """h = {}
 
         for n in nums:
             h[n] = h.get(n,0)+1
@@ -9,6 +9,20 @@ class Solution:
 
         for key, value in h.items():
             if value == l:
-                return key
+                return key"""
         
+        can = nums[0]
+        count = 1
+
+        for i in range(1,len(nums)):
+            if count != 0:
+                if nums[i] == can:
+                    count+=1
+                else:
+                    count-=1
+            else:
+                can = nums[i]
+                count+=1
         
+        return can
+            
