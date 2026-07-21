e68a4752065017441107abd1f8f9a21512002dc9
Shivprasad Devkatte <shivprasaddevkatte2807@gmail.com>
2026-07-21 20:20:55 +0530

    Time: 0 ms (100%), Space: 19.4 MB (16.76%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+
+        memo = {}
+
+        def fib(n):
+            if n == 0 or n == 1:
+                return 1
+            
+            if n in memo:
+                return memo[n]
+
+            memo[n] = fib(n-1) + fib(n-2)
+            return memo[n]
+
+        return fib(n)
